334ba19f2c71045d64b74a3e61b1adc032b0a0e8
Lucas-Wilian <lucaswilianr.s@gmail.com>
2022-06-04 18:15:46 -0300

    Sexto commit: Estilização das Thumbnail/scroll bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import { Banner } from '../components/Banner/Banner';
 import { Header } from '../components/Header/Header';
+import Row from '../components/Row/Row';
 import { Movie } from '../typings';
 import requests from '../utils/requests';
 
@@ -27,15 +28,27 @@ const Home = ({
   trendingNow,
 }: Props) => {
   return (
-    <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]'>
+    <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] '>
       <Head>
         <title>Home - NetFlix</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
+
       <Header />
-      <main>
+
+      <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16  '>
         <Banner netflixOriginals={netflixOriginals} />
 
-        <section></section>
+        <section className='md:space-y-24'>
+          <Row title='Trending Now' movies={trendingNow} />
+          <Row title='Top Rated' movies={topRated} />
+          <Row title='Action Thrillers' movies={actionMovies} />
+
+          <Row title='Comedies' movies={comedyMovies} />
+          <Row title='Scary Movies' movies={horrorMovies} />
+          <Row title='Romance Movies' movies={romanceMovies} />
+          <Row title='Documentaries' movies={documentaries} />
+        </section>
       </main>
     </div>
   );
