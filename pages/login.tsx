96a08907612e7ac5c4da7d0d4486cac56b7859dd
Lucas-Wilian <lucaswilianr.s@gmail.com>
2022-06-05 18:19:29 -0300

    Oitavo commit: iniciando a autenticacao

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,19 @@
+import Head from 'next/head';
 import React from 'react';
+import Image from 'next/image';
 
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Head>
+        <title>Netflix</title>
+      </Head>
+      <Image
+        src='https://rb.gy/p2hghi'
+        layout='fill'
+        className='-z-10 !hidden opacity-60 sm:inline'
+        objectFit='cover'
+      />
+    </div>
+  );
 };
