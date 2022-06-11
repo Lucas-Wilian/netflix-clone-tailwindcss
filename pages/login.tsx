d022264a0d73e27f5d0dfbb5751e062d24cb4e7e
Lucas-Wilian <lucaswilianr.s@gmail.com>
2022-06-10 22:57:19 -0300

    Nono commit: React-Hook-Form

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,25 @@
 import Head from 'next/head';
 import React from 'react';
 import Image from 'next/image';
+import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface Inputs {
+  email: string;
+  password: string;
+}
 
 function Login() {
+  const [login, setLogin] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <div className='relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent'>
       <Head>
@@ -21,14 +38,27 @@ function Login() {
         width={150}
         height={150}
       />
-      <form className='relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14'>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className='relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14'
+      >
         <h1 className='text-4xl font-semibold'>Sign In</h1>
         <div className='space-y-4'>
           <label className='inline-block w-full'>
-            <input type='email' placeholder='Email' className='input' />
+            <input
+              {...register('email', { required: true })}
+              type='email'
+              placeholder='Email'
+              className='input'
+            />
           </label>
           <label className='inline-block w-full'>
-            <input type='password' placeholder='PassWord' className='input' />
+            <input
+              {...register('password', { required: true })}
+              type='password'
+              placeholder='PassWord'
+              className='input'
+            />
           </label>
 
           <button className='w-full rounded bg-[#E50914] py-3 font-semibold'>
